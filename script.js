688fe2e31f002be2bf5b49015992c6a5a1fe0908
leonpoh0 <leonpoh0@gmail.com>
2022-04-22 10:49:49 +0800

    enhancements with Bootstrap

--- script.js
+++ script.js
@@ -8,9 +8,8 @@ const delayInMilliseconds = 1000;
 
 const getRandomIndex = (max) => Math.floor(Math.random() * max);
 
-let matchPattern = document.createElement("p");
-matchPattern.classList = "gameStatusMessage";
-document.body.appendChild(matchPattern);
+let gameStatusMessage = document.getElementById("gameStatusMessageId");
+let creditMessage = document.getElementById("creditMessageId");
 
 let payTable = {
   "Royal Flush": 800,
@@ -28,6 +27,21 @@ let payTable = {
 const winCondition = () => {
   playerHand.sort((a, b) => a.rank - b.rank);
   let firstRank = playerHand[0].rank;
+  let rankTally = {};
+  for (let i = 0; i < 5; i += 1) {
+    let rankOfCard = playerHand[i].rank;
+    if (rankOfCard in rankTally) {
+      rankTally[rankOfCard] += 1;
+    } else {
+      rankTally[rankOfCard] = 1;
+    }
+  }
+  // put counts of same ranks in an array and sort them
+  let uniqueRankCounters = [];
+  for (rankOfCard in rankTally) {
+    uniqueRankCounters.push(rankTally[rankOfCard]);
+  }
+  uniqueRankCounters.sort((a, b) => b - a);
   if (
     playerHand[0].suit === playerHand[1].suit &&
     playerHand[1].suit === playerHand[2].suit &&
@@ -52,93 +66,80 @@ const winCondition = () => {
         playerHand[3].rank === firstRank + 3 &&
         playerHand[4].rank === firstRank + 4
       ) {
-        return 50;
-      } else if (
-        firstRank === 1 &&
-        playerHand[1].rank === 10 &&
-        playerHand[2].rank === 11 &&
-        playerHand[3].rank === 12 &&
-        playerHand[4].rank === 13
-      ) {
         return "Straight Flush";
       }
     }
   } else {
-    let rankTally = {};
-    for (let i = 0; i < 5; i += 1) {
-      let rankOfCard = playerHand[i].rank;
-      if (rankOfCard in rankTally) {
-        rankTally[rankOfCard] += 1;
-      } else {
-        rankTally[rankOfCard] = 1;
-      }
-    }
-    // put counts of same ranks in an array and sort them
-    let uniqueRankCounters = [];
-    for (rankOfCard in rankTally) {
-      uniqueRankCounters.push(rankTally[rankOfCard]);
-    }
-    uniqueRankCounters.sort((a, b) => b - a);
     // check for Four of a Kind
     if (uniqueRankCounters[0] === 4 && uniqueRankCounters[1] === 1) {
       return "Four of a kind";
     } // check for Full House
     else if (uniqueRankCounters[0] === 3 && uniqueRankCounters[1] === 2) {
       return "Full House";
-    } // check for Flush
-    else if (
-      playerHand[0].suit === playerHand[1].suit &&
-      playerHand[1].suit === playerHand[2].suit &&
-      playerHand[2].suit === playerHand[3].suit &&
-      playerHand[3].suit === playerHand[4].suit &&
-      playerHand[4].suit === playerHand[0].suit
-    ) {
-      return "Flush";
-    } // check for Straight
-    else if (
-      playerHand[1].rank === firstRank + 1 &&
-      playerHand[2].rank === firstRank + 2 &&
-      playerHand[3].rank === firstRank + 3 &&
-      playerHand[4].rank === firstRank + 4
-    ) {
-      return "Straight";
-    } else if (
-      firstRank === 1 &&
-      playerHand[1].rank === 10 &&
-      playerHand[2].rank === 11 &&
-      playerHand[3].rank === 12 &&
-      playerHand[4].rank === 13
-    ) {
-      return 4;
-    } // check for Three of a Kind
-    else if (
-      uniqueRankCounters[0] === 3 &&
-      uniqueRankCounters[1] === 1 &&
-      uniqueRankCounters[2] === 1
-    ) {
-      return "Three of a Kind";
-    } // check for Two Pairs
-    else if (
-      uniqueRankCounters[0] === 2 &&
-      uniqueRankCounters[1] === 2 &&
-      uniqueRankCounters[2] === 1
-    ) {
-      return "Two Pairs";
-    } // check for Jacks or Better
-    else if (
-      rankOfCard[11] === 2 ||
-      rankOfCard[12] === 2 ||
-      rankOfCard[13] === 2 ||
-      rankOfCard[1] === 2
-    ) {
-      return "Jacks or Better";
     }
+  } // check for Flush
+  if (
+    playerHand[0].suit === playerHand[1].suit &&
+    playerHand[1].suit === playerHand[2].suit &&
+    playerHand[2].suit === playerHand[3].suit &&
+    playerHand[3].suit === playerHand[4].suit &&
+    playerHand[4].suit === playerHand[0].suit
+  ) {
+    return "Flush";
+  } // check for Straight - 2 cases
+  else if (
+    playerHand[1].rank === firstRank + 1 &&
+    playerHand[2].rank === firstRank + 2 &&
+    playerHand[3].rank === firstRank + 3 &&
+    playerHand[4].rank === firstRank + 4
+  ) {
+    return "Straight";
+  } else if (
+    firstRank === 1 &&
+    playerHand[1].rank === 10 &&
+    playerHand[2].rank === 11 &&
+    playerHand[3].rank === 12 &&
+    playerHand[4].rank === 13
+  ) {
+    return "Straight";
+  } // check for Three of a Kind
+  else if (
+    uniqueRankCounters[0] === 3 &&
+    uniqueRankCounters[1] === 1 &&
+    uniqueRankCounters[2] === 1
+  ) {
+    return "Three of a Kind";
+  } // check for Two Pairs
+  else if (
+    uniqueRankCounters[0] === 2 &&
+    uniqueRankCounters[1] === 2 &&
+    uniqueRankCounters[2] === 1
+  ) {
+    return "Two Pairs";
+  } // check for Jacks or Better
+  else if (
+    rankTally[11] === 2 ||
+    rankTally[12] === 2 ||
+    rankTally[13] === 2 ||
+    rankTally[1] === 2
+  ) {
+    return "Jacks or Better";
   }
   return "Nothing";
 };
 
 function calcHandScore() {
   return payTable[winCondition()];
+}
+
+function getCard(i) {
+  let cardId = "card" + String(i);
+  return document.getElementById(cardId);
+}
+
+function getCardHoldIndicator(i) {
+  let holdId = "hold" + String(i);
+  return document.getElementById(holdId);
 }
 
 const shuffleCards = (cards) => {
@@ -209,181 +210,129 @@ const createDeck = () => {
   return newDeck;
 };
 
-const holdCard = (i, holdIndicator) => {
-  if (playerBtn.innerText === "DRAW") {
+const holdCard = (i) => {
+  let cardHoldIndicator = getCardHoldIndicator(i);
+  if (actionBtn.innerText === "DRAW") {
     if (holdCardsHand[i] === 0) {
       holdCardsHand[i] = 1;
-      holdIndicator.innerText = "Hold";
+      cardHoldIndicator.innerText = "Hold";
+      cardHoldIndicator.classList.add("holdIndicatorBackground");
     } else {
       holdCardsHand[i] = 0;
-      holdIndicator.innerText = "";
+      cardHoldIndicator.innerText = "";
+      cardHoldIndicator.classList = "holdIndicator";
     }
   }
 };
 
 const drawCards = () => {
-  if (playerBtn.innerText === "DEAL") {
+  if (actionBtn.innerText === "DEAL") {
     for (let i = 0; i < 5; i += 1) {
       let cardDrawn = deck.pop();
       playerHand.push(cardDrawn);
+      createCards(i);
     }
-    playerBtn.innerText = "DRAW";
-    matchPattern.innerText = "Select cards to hold and then draw cards.";
-    createCards();
-  } else if (playerBtn.innerText === "DRAW") {
+    actionBtn.innerText = "DRAW";
+    gameStatusMessage.innerText = "Select cards to hold and then draw cards.";
+  } else if (actionBtn.innerText === "DRAW") {
     for (let i = 0; i < 5; i += 1) {
       if (holdCardsHand[i] === 0) {
         let cardDrawn = deck.pop();
         playerHand[i] = cardDrawn;
+        createCards(i);
       } else {
         holdCardsHand[i] = 0; // reset to default 0
       }
-      createCards();
     }
     let score = calcHandScore();
     credits += score;
     creditMessage.innerText = "CREDITS: " + credits;
     if (winCondition() === "Nothing") {
-      matchPattern.innerText = "You lost this round.";
+      gameStatusMessage.innerText = "You lost this round.";
     } else {
-      matchPattern.innerText = winCondition();
+      gameStatusMessage.innerText = winCondition();
     }
-    console.log(credits);
     const ref = setInterval(() => {
       if (milliseconds >= 3000) {
-        if (playerBtn.innerText === "TRY AGAIN") {
-          matchPattern.innerText = "Better luck next time. Try again.";
+        if (actionBtn.innerText === "TRY AGAIN") {
+          if (gameStatusMessage.innerText === "You lost this round.") {
+            gameStatusMessage.innerText = "Better luck next time. Try again.";
+          } else {
+            gameStatusMessage.innerText = "Play again?";
+          }
         }
-        console.log(milliseconds);
         clearInterval(ref);
       }
       milliseconds += 1000;
     }, delayInMilliseconds);
-    playerBtn.innerText = "TRY AGAIN";
-  } else if (playerBtn.innerText === "TRY AGAIN") {
-    console.log("test");
-    playerBtn.innerText = "DEAL";
+    actionBtn.innerText = "TRY AGAIN";
+  } else if (actionBtn.innerText === "TRY AGAIN") {
+    actionBtn.innerText = "DEAL";
     initGame();
   }
 };
 
-const createCards = () => {
-  cardsContainer.innerHTML = "";
-  for (let i = 0; i < 5; i += 1) {
-    const suit = document.createElement("div");
-    suit.classList.add("suitCSS", playerHand[i].colour);
-    suit.innerText = playerHand[i].suit;
+const createCards = (i) => {
+  let cardAtPosition = getCard(i);
+  cardAtPosition.innerHTML = "";
+  cardAtPosition.classList = "filledCard";
 
-    const name = document.createElement("div");
-    name.classList.add("nameCSS", playerHand[i].colour);
-    name.innerText = playerHand[i].displayName;
+  const suit = document.createElement("div");
+  suit.classList.add("suitCSS", playerHand[i].colour);
+  suit.innerText = playerHand[i].suit;
 
-    let holdIndicator = document.createElement("div");
-    holdIndicator.classList = "holdIndicator cardWithHoldIndicatorCSS";
-    holdIndicator.innerText = "";
+  const name = document.createElement("div");
+  name.classList.add("nameCSS", playerHand[i].colour);
+  name.innerText = playerHand[i].displayName;
 
-    const card = document.createElement("div");
-    card.classList.add("cardCSS");
-
-    const cardWithHoldIndicator = document.createElement("div");
-    cardWithHoldIndicator.classList = "cardWithHoldIndicatorCSS";
-    cardWithHoldIndicator.addEventListener("click", () =>
-      holdCard(i, holdIndicator)
-    );
-
-    card.appendChild(name);
-    card.appendChild(suit);
-    cardWithHoldIndicator.append(card);
-    cardWithHoldIndicator.append(holdIndicator);
-    cardsContainer.append(cardWithHoldIndicator);
-  }
+  cardAtPosition.append(suit, name);
+  cardAtPosition.addEventListener("click", () => holdCard(i));
 };
 
-function popModal() {
-  var payTableModal = document.createElement("div");
-  payTableModal.classList = "modal";
-  document.body.appendChild(payTableModal);
-  let payTableInfo = document.createElement("table");
-  let thead = document.createElement("thead");
-  let tbody = document.createElement("tbody");
-  let closeBtn = document.createElement("span");
-  closeBtn.innerText = "×";
-  closeBtn.classList = "close";
-  closeBtn.addEventListener(
-    "click",
-    () => (payTableModal.style.display = "none")
-  );
-  /*
-  window.addEventListener("click", (event) => {
-    if (event.target.classList === "modal") {
-      if (payTableModal) {
-        payTableModal.style.display = "none";
-      }
-    }
-  });
-*/
+let payTableModal = document.getElementById("modal-body");
+let payTableInfo = document.createElement("table");
+let thead = document.createElement("thead");
+let tbody = document.createElement("tbody");
+payTableInfo.appendChild(thead);
+payTableInfo.appendChild(tbody);
+payTableModal.appendChild(payTableInfo);
 
-  payTableModal.appendChild(closeBtn);
-  payTableInfo.appendChild(thead);
-  payTableInfo.appendChild(tbody);
-  payTableModal.appendChild(payTableInfo);
+let headingRow = document.createElement("tr");
+let heading1 = document.createElement("th");
+heading1.innerText = "Poker Hand";
+let heading2 = document.createElement("th");
+heading2.innerText = "Score";
+headingRow.append(heading1, heading2);
+thead.append(headingRow);
 
-  let headingRow = document.createElement("tr");
-  let heading1 = document.createElement("th");
-  heading1.innerText = "Poker Hand";
-  let heading2 = document.createElement("th");
-  heading2.innerText = "Score";
-  headingRow.append(heading1, heading2);
-  thead.append(headingRow);
-
-  for (i in payTable) {
-    const conditionRow = document.createElement("tr");
-    const pokerCondition = document.createElement("td");
-    const pokerHandScore = document.createElement("td");
-    pokerCondition.innerText = i;
-    pokerHandScore.innerText = payTable[i];
-    conditionRow.append(pokerCondition, pokerHandScore);
-    tbody.appendChild(conditionRow);
-  }
-  payTableModal.style.display = "block";
+for (i in payTable) {
+  const conditionRow = document.createElement("tr");
+  const pokerCondition = document.createElement("td");
+  const pokerHandScore = document.createElement("td");
+  pokerCondition.innerText = i;
+  pokerHandScore.innerText = payTable[i];
+  conditionRow.append(pokerCondition, pokerHandScore);
+  tbody.appendChild(conditionRow);
 }
+payTableModal.style.display = "block";
 
-let cardsContainer = document.createElement("div");
-cardsContainer.classList = "cardsContainerCSS";
-document.body.appendChild(cardsContainer);
-
-let creditMessage = document.createElement("div");
-creditMessage.classList = "creditMessage";
-document.body.appendChild(creditMessage);
-
-let bottomSection = document.createElement("div");
-bottomSection.classList = "bottomSection";
-document.body.appendChild(bottomSection);
-
-const playerBtn = document.createElement("button");
-playerBtn.addEventListener("click", drawCards);
-playerBtn.classList = "playerBtn";
-bottomSection.appendChild(playerBtn);
-
-const payTableBtn = document.createElement("button");
-payTableBtn.addEventListener("click", popModal);
-payTableBtn.classList = "payTableBtn";
-payTableBtn.innerText = "PAY TABLE";
-bottomSection.appendChild(payTableBtn);
+const actionBtn = document.getElementById("actionBtn");
+actionBtn.addEventListener("click", drawCards);
 
 function initGame() {
-  cardsContainer.innerHTML = "";
+  for (let i = 0; i < 5; i += 1) {
+    let cardAtPosition = getCard(i);
+    cardAtPosition.classList = "emptyCard";
+    cardAtPosition.innerHTML = "";
+    let cardHoldIndicator = getCardHoldIndicator(i);
+    cardHoldIndicator.classList = "holdIndicator";
+    cardHoldIndicator.innerText = "";
+  }
   creditMessage.innerText = "CREDITS: " + credits;
-  matchPattern.innerText = "Press deal to start.";
-  playerBtn.innerText = "DEAL";
+  gameStatusMessage.innerHTML = "Press deal to start.";
+  actionBtn.innerText = "DEAL";
   deck = shuffleCards(createDeck());
   playerHand = [];
-
-  for (let i = 0; i < 5; i += 1) {
-    const emptyCard = document.createElement("div");
-    emptyCard.classList = "emptyCard";
-    cardsContainer.appendChild(emptyCard);
-  }
 }
 
 initGame();
